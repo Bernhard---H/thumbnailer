263b645b01efea3c38201341889e92e8a2800fe0
Bernhard---H <halbart.bernhard+git@gmail.com>
2014-09-28 17:11:52 +0200

    added CLI help

---
+++ src/cli/help.php
@@ -0,0 +1,8 @@
+Thumbnailer VERSION by AUTHOR
+
+Usage: php thumbnailer.phar [options]
+
+Options:
+
+    -h, --help
+        Display this help message.
--- src/cli/index.php
+++ src/cli/index.php
@@ -5,3 +5,9 @@
 
 require_once 'phar://thumbnailer.phar/common/loader.php';
 
+
+$arguments = getopt('h', array('help'));
+
+if (isset($arguments['h']) || isset($arguments['help']) || empty($arguments)) {
+    include BASE_DIR.'/cli/help.php';
+}
